005d3179cfbcab9d2c1f1d38c9d845635744a57e
tikomo <tikomo@Komorita-no-MacBook-Air.local>
2014-02-05 01:21:41 +0900

    commit

--- Sample020402/ViewController.m
+++ Sample020402/ViewController.m
@@ -8,7 +8,23 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    
+}
+
+// GUI部品は他のクラスからアクセス出来なくていいのでここにControl + ドラッグする
+
+// 左手でControl Key + track pad, 右手でストーリーボードから.mへ移動かな〜
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+- (IBAction)button1_onClick:(id)sender;
+- (IBAction)button2_onClick:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *textField1;
+@property (weak, nonatomic) IBOutlet UISlider *slider1;
+- (IBAction)slider1_valueChanged:(id)sender;
+- (IBAction)slider2_valueChanged:(UISlider *)sender;
+@property (weak, nonatomic) IBOutlet UITextField *textField2;
+
 
 @end
 
@@ -26,4 +42,36 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)button1_onClick:(id)sender {
+    NSLog(@"button1_onClick");
+    _textField1.text = @"Hello World(1)";
+    _textField1.textColor = [UIColor redColor];
+}
+
+- (IBAction)button2_onClick:(id)sender {
+    NSLog(@"button2_onClick");
+    _textField1.text = @"Hello World(2)";
+    _textField1.textColor = [UIColor blueColor];
+}
+- (IBAction)slider1_valueChanged:(id)sender {
+    
+    // case1
+    // NSLog(@"%.1f %%", _slider1.value * 100);
+    
+    // case2
+    // NSLog(@"%.1f %%", ((UISlider *)sender).value * 100);
+    
+    // case3
+    UISlider *mySlider = sender;
+    NSLog(@"%.1f %%", mySlider.value * 100);
+    
+    
+    _textField1.text = [NSString stringWithFormat:@"%.2f" , ((UISlider *)sender).value];
+    
+    
+}
+
+- (IBAction)slider2_valueChanged:(UISlider *)sender {
+    _textField2.text = [NSString stringWithFormat:@"%.2f" , sender.value];
+}
 @end
