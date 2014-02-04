6490f77159558da9b84f9ad0e51e19a277da3bff
tikomo <tikomo@Komorita-no-MacBook-Air.local>
2014-02-04 21:28:02 +0900

    Initial Commit

---
+++ Sample020402/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sample020402
+//
+//  Created by tikomo on 2014/02/04.
+//  Copyright (c) 2014年 tikomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Sample020402Tests/Sample020402Tests.m
@@ -0,0 +1,34 @@
+//
+//  Sample020402Tests.m
+//  Sample020402Tests
+//
+//  Created by tikomo on 2014/02/04.
+//  Copyright (c) 2014年 tikomo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Sample020402Tests : XCTestCase
+
+@end
+
+@implementation Sample020402Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
